8aba44c85d7183e03023a1e0b6579d6ee558d1d2
7Sageer <125936732+7Sageer@users.noreply.github.com>
2023-04-20 21:59:59 +0800

    初始化，just试试

---
+++ src/model/Chessboard.java
@@ -0,0 +1,86 @@
+package model;
+
+/**
+ * This class store the real chess information.
+ * The Chessboard has 9*7 cells, and each cell has a position for chess
+ */
+public class Chessboard {
+    private Cell[][] grid;
+
+    public Chessboard() {
+        this.grid =
+                new Cell[Constant.CHESSBOARD_ROW_SIZE.getNum()][Constant.CHESSBOARD_COL_SIZE.getNum()];//19X19
+
+        initGrid();
+        initPieces();
+    }
+
+    private void initGrid() {
+        for (int i = 0; i < Constant.CHESSBOARD_ROW_SIZE.getNum(); i++) {
+            for (int j = 0; j < Constant.CHESSBOARD_COL_SIZE.getNum(); j++) {
+                grid[i][j] = new Cell();
+            }
+        }
+    }
+
+    private void initPieces() {
+        grid[0][0].setPiece(new ChessPiece(PlayerColor.BLUE, "Elephant",8));
+        grid[8][6].setPiece(new ChessPiece(PlayerColor.RED, "Elephant",8));
+    }
+
+    private ChessPiece getChessPieceAt(ChessboardPoint point) {
+        return getGridAt(point).getPiece();
+    }
+
+    private Cell getGridAt(ChessboardPoint point) {
+        return grid[point.getRow()][point.getCol()];
+    }
+
+    private int calculateDistance(ChessboardPoint src, ChessboardPoint dest) {
+        return Math.abs(src.getRow() - dest.getRow()) + Math.abs(src.getCol() - dest.getCol());
+    }
+
+    private ChessPiece removeChessPiece(ChessboardPoint point) {
+        ChessPiece chessPiece = getChessPieceAt(point);
+        getGridAt(point).removePiece();
+        return chessPiece;
+    }
+
+    private void setChessPiece(ChessboardPoint point, ChessPiece chessPiece) {
+        getGridAt(point).setPiece(chessPiece);
+    }
+
+    public void moveChessPiece(ChessboardPoint src, ChessboardPoint dest) {
+        if (!isValidMove(src, dest)) {
+            throw new IllegalArgumentException("Illegal chess move!");
+        }
+        setChessPiece(dest, removeChessPiece(src));
+    }
+
+    public void captureChessPiece(ChessboardPoint src, ChessboardPoint dest) {
+        if (isValidCapture(src, dest)) {
+            throw new IllegalArgumentException("Illegal chess capture!");
+        }
+        // TODO: Finish the method.
+    }
+
+    public Cell[][] getGrid() {
+        return grid;
+    }
+    public PlayerColor getChessPieceOwner(ChessboardPoint point) {
+        return getGridAt(point).getPiece().getOwner();
+    }
+
+    public boolean isValidMove(ChessboardPoint src, ChessboardPoint dest) {
+        if (getChessPieceAt(src) == null || getChessPieceAt(dest) != null) {
+            return false;
+        }
+        return calculateDistance(src, dest) == 1;
+    }
+
+
+    public boolean isValidCapture(ChessboardPoint src, ChessboardPoint dest) {
+        // TODO:Fix this method
+        return false;
+    }
+}
