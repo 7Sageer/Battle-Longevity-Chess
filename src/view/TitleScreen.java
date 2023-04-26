3a3301107be0ae91a3e13ba5562f2a87d5c98acd
戚涵睿 <12210216@mail.sustech.edu.cn>
2023-04-26 15:16:48 +0800

    插入了一张图片

--- src/view/TitleScreen.java
+++ src/view/TitleScreen.java
@@ -9,16 +9,16 @@ import model.Chessboard;
 
 public class TitleScreen extends JFrame {
     private JButton startButton;
+    private JButton exitButton;
 
     public TitleScreen() {
         super("Chess Game");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setPreferredSize(new Dimension(400, 300));
+        setPreferredSize(new Dimension(400, 250));
 
-        JPanel panel = new JPanel(new BorderLayout());
+        JPanel panel = new JPanel(new FlowLayout());
         JLabel titleLabel = new JLabel("Welcome to Chess Game!");
         titleLabel.setHorizontalAlignment(JLabel.CENTER);
-        panel.add(titleLabel, BorderLayout.CENTER);
 
         startButton = new JButton("Start Game");
         startButton.addActionListener(new ActionListener() {
@@ -31,7 +31,26 @@ public class TitleScreen extends JFrame {
                 dispose();
             }
         });
-        panel.add(startButton, BorderLayout.SOUTH);
+        
+
+        exitButton = new JButton("Exit Game");
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        ImageIcon imageIcon = new ImageIcon("resource\\Elephant-red.png");
+        JLabel imageLabel = new JLabel(imageIcon);
+        panel.add(imageLabel);
+
+        //startButton.setBounds(100, 100, 50, 50);需要四个参数，分别是按钮的 x 坐标、y 坐标、宽度和高度
+
+        panel.add(titleLabel);
+        panel.add(startButton);
+        panel.add(exitButton);
+        
 
         getContentPane().add(panel);
         pack();
