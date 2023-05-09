f40d100e5a49ec1df9aa0422a8ae3e3eeef93e63
QwQZZZZZ <92218403+QwQZZZZZ@users.noreply.github.com>
2023-05-09 20:53:21 +0800

    Update GameController.java
    增加了一个Player的getter

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -323,3 +323,6 @@ public class GameController implements GameListener {
     public int getAIDepth(){return AIDepth;}
 
 }
+public PlayerColor getCurrentPlayer(){//refresh currentPlay to TimeLimit
+        return currentPlayer;
+    }
