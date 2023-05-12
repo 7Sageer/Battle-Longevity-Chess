9db74753a9def8576a6c13013733c8edbbf3fc14
QwQZZZZZ <92218403+QwQZZZZZ@users.noreply.github.com>
2023-05-12 23:47:11 +0800

    Update ClientDemo.java
    可以传输对象，但目前仅单向

--- src/controller/ClientDemo.java
+++ src/controller/ClientDemo.java
@@ -1,5 +1,12 @@
 package controller;
 
+import com.sun.tools.javac.Main;
+import resourcePlayer.BGM;
+import resourcePlayer.FontsManager;
+import view.TitleScreen;
+import model.Chessboard;
+
+import javax.swing.*;
 import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
@@ -11,24 +18,45 @@ import java.net.Socket;
  * 3.释放资源
  */
 public class ClientDemo {
+
     public static void main(String[] args) throws IOException {
+        Thread thread = new Thread(() -> {
+            SwingUtilities.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    new BGM().playMusic("resource\\bgm.wav");
+                    FontsManager.PixelFonts();
+                    new TitleScreen();
+                }
+            });
+        });
+        thread.start();
         // 创建客户端socket对象
         // 创建流套接字并将其连接到指定IP地址的指定端口号
-        Socket socket = new Socket(InetAddress.getByName("127.0.0.1"),9002);
+        Socket socket = new Socket(InetAddress.getByName("127.0.0.1"), 9002);
         // 获取输出流,写数据
         System.out.println("input you data");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         // 封装输出流对象
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-        String line ;
+        String line;
         //获取输出流，写数据；
-        while ((line = br.readLine()) != null){
+        while ((line = br.readLine()) != null) {
             // 获取输出流对象
             bw.write(line);
             bw.newLine();
             bw.flush();
         }
+        //以对象形式输出
+        FileInputStream fileInputStream = new FileInputStream("temp.txt");
+        while (fileInputStream != null) {
+            ObjectOutput objectOutput = new ObjectOutputStream(socket.getOutputStream());
+            Chessboard chessboard = new Chessboard();
+            objectOutput.writeObject(chessboard.historyAction);
+            objectOutput.flush();
+        }
         // 释放资源
         socket.close();
     }
 }
+
