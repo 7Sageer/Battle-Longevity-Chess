4cbe29a7859405a85e97deb00b131d9063911a01
QwQZZZZZ <92218403+QwQZZZZZ@users.noreply.github.com>
2023-05-12 23:48:14 +0800

    Update ServerDemo.java
    可以接收对象了，但目前单向

--- src/controller/ServerDemo.java
+++ src/controller/ServerDemo.java
@@ -1,8 +1,16 @@
 package controller;
 
+import resourcePlayer.BGM;
+import resourcePlayer.FontsManager;
+import view.CellComponent;
+import view.TitleScreen;
+
+import javax.swing.*;
 import java.io.*;
 import java.net.Socket;
 import java.net.ServerSocket;
+import model.Chessboard;
+
 /**
  * TCP 接收数据步骤
  * 1.创建服务端socket对象
@@ -12,6 +20,17 @@ import java.net.ServerSocket;
  */
 public class ServerDemo {
     public static void main(String[] args) throws IOException {
+        Thread thread = new Thread(() -> {
+            SwingUtilities.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    new BGM().playMusic("resource\\bgm.wav");
+                    FontsManager.PixelFonts();
+                    new TitleScreen();
+                }
+            });
+        });
+        thread.start();
         // 创建服务端socket对象
         ServerSocket serverSocket = new ServerSocket(9002);
         // 监听客户端连接,返回一个socket对象
@@ -24,5 +43,12 @@ public class ServerDemo {
         }
         // 释放资源
         serverSocket.close();
+        //接收ChessBoard对象
+        try {
+            ObjectInput objectInput = new ObjectInputStream(socket.getInputStream());
+            Chessboard chessboard = (Chessboard) objectInput.readObject();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
