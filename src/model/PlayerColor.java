9f7956b46d81813e76aeb7ad710752a322e21849
QwQZZZZZ <92218403+QwQZZZZZ@users.noreply.github.com>
2023-05-23 20:45:05 +0800

    全体序列化

--- src/model/PlayerColor.java
+++ src/model/PlayerColor.java
@@ -1,12 +1,13 @@
 package model;
 
 import java.awt.*;
+import java.io.Serializable;
 
 /**
  * This class is mainly used to wrap the Color object that describes
  * the ownership of the piece and the current player.
  */
-public enum PlayerColor {
+public enum PlayerColor implements Serializable {
     BLUE(Color.BLUE), RED(Color.RED);
 
     private final Color color;
